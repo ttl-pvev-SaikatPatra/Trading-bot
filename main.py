01df1c340af86cb33b12e5377ecc9dfda6cd54bc
ttl-pvev-SaikatPatra <119476844+ttl-pvev-SaikatPatra@users.noreply.github.com>
2025-08-28 16:46:37 +0530

    tried to suppress the error  of dataframe

--- main.py
+++ main.py
@@ -256,9 +256,10 @@ class AutoTradingBot:
             g["Ret20"] = g["Close"].pct_change(20, fill_method=None)
             g["GapRisk"] = (g["Open"] - g["Close"].shift(1)).abs().rolling(20).std()
             return g
-        df = df.groupby("Symbol", group_keys=False).apply(lambda g: featurize(g.drop(columns=["Symbol"]))
-.assign(Symbol=g["Symbol"].iloc[0])
+        df = df.groupby("Symbol", group_keys=False).apply(
+    lambda g: featurize(g.drop(columns=["Symbol"])).assign(Symbol=g["Symbol"].iloc[0])
 )
+
         last = df.sort_values(["Symbol","Date"]).groupby("Symbol").tail(1)
         last["ATRpct_rank"] = last["ATR_pct"].rank(pct=True)
         last["Turn_rank"] = last["MedTurn20"].rank(pct=True)
